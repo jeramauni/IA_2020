4000b4725e75024b8e9429796542a60feb9a3c33
Marcos Docampo <marcosdo@ucm.es>
2020-02-24 22:52:20 +0100

    Actualizado Rata.cs
    Ahora persigue pero no se orienta.

--- Movimiento/Assets/Scripts/ScriptsPractica1/Rata.cs
+++ Movimiento/Assets/Scripts/ScriptsPractica1/Rata.cs
@@ -5,63 +5,63 @@ namespace UCM.IAV.Movimiento
     public class Rata : MonoBehaviour
     {
         // KINEMATIC
-        private Vector3 position;
-        private Vector3 velocity;
-        public float orientation;
-        public float rotation;
-        private float time_;
-        protected Direccion steering_;
+        public float maxVelocity = 2.0f;
+        protected struct Dir {
+            public Quaternion angle;
+            public Vector3 direc;
+            public Dir(Quaternion a, Vector3 d) {
+                angle = a;
+                direc = d;
+            }
+        };
+        private Dir dir;
         private Rigidbody rb_;
         // KINEMATIC SEEK
         public GameObject target;
-        public float maxSpeed;
-        // ARRIVE
-        public float radius;
-        public float timeToTarget;
         //-----------------------------------------------------------------------
         private void Awake() {
             rb_ = GetComponent<Rigidbody>();
         }
-        private void Start() {
-            position = this.transform.position;
-            velocity = rb_.velocity;
-            time_ = Time.deltaTime;
-            timeToTarget = 0.25f;
-        }
         private void Update() {
+            dir = getSteering();
+
+            Debug.Log("Rotacion: " + rb_.rotation + "\n");
+            Debug.Log("Velocidad: " + rb_.velocity + "\n");
+            
             // Modificar la posicion y la orientacion
-            float half_t_sq = 0.5f * time_ * time_;
-            position += velocity * time_ + steering_.lineal * half_t_sq;
-            orientation += rotation * time_ + steering_.angular * half_t_sq;
+            float time = Time.deltaTime;
+            float half_t_sq = 0.5f * time * time;
+            this.transform.position += rb_.velocity * time + dir.direc * half_t_sq;
 
             // y la velocidad y la rotation
-            velocity += steering_.lineal * time_;
-            rotation += steering_.angular * time_;
+            rb_.velocity += dir.direc * time;
         }
+
         // Funcion para modificar la orientacion segun la velocidad
-        private float newOrientation(float current, Vector3 vel) {
-            // Comprobar que hay velocidad
-            if (vel.magnitude > 0) {
-                // Calcular la orientacion dada la velocidad
-                return Vector3.Angle(new Vector3(1.0f, 0.0f, 0.0f), vel);
-            }
-            // Si no, usar la orientación actual
-            else return current;
-        }
-        // BUSQUEDA Y LLEGADA
-        private Direccion getSteering() {
-            Direccion result = new Direccion();
+        // private float newOrientation(float current, Vector3 vel) {
+        //     // Comprobar que hay velocidad
+        //     if (vel.magnitude > 0) {
+        //         // Calcular la orientacion dada la velocidad
+        //         return Vector3.Angle(new Vector3(1.0f, 0.0f, 0.0f), vel);
+        //     }
+        //     // Si no, usar la orientación actual
+        //     else return current;
+        // }
+
+        // BUSQUEDA
+        private Dir getSteering() {
+            Dir result = new Dir();
             // Coger la direccion al objetivo
-            result.lineal =  target.transform.position - this.transform.position;
+            result.direc =  target.transform.position - this.transform.position;
 
             // La velocidad va a lo largo de esta direccion, a toda velocidad
-            result.lineal.Normalize();
-            result.lineal *= maxSpeed;
+            result.direc.Normalize();
+            result.direc *= maxVelocity;
 
             // Poner en la direccion que queremos que vaya
-            this.orientation = newOrientation(this.orientation, result.lineal);
+            //this.orientation = newOrientation(this.orientation, result.lineal);
 
-            result.angular = 0;
+            result.angle = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
             return result;
         }
     }
