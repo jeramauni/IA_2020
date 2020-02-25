c5774a88b6d3627e46f20e06a991b1901a895bc2
Marcos Docampo <marcosdo@ucm.es>
2020-02-25 02:15:19 +0100

    Mejorada Rata seguidora
    Ahora ya cumple el tiempo de llegada y la velocidad máxima

--- Movimiento/Assets/Scripts/ScriptsPractica1/Rata.cs
+++ Movimiento/Assets/Scripts/ScriptsPractica1/Rata.cs
@@ -5,15 +5,15 @@ namespace UCM.IAV.Practica1
     public class Rata : MonoBehaviour
     {
         // KINEMATIC
-        public float maxVelocity = 0.0f;
         public float radius = 2.0f;
-        private float timeToTarget = 0.25f;
+        public float maxVelocity = 1.0f;
+        public float timeToTarget = 0.25f;
         protected struct Dir {
             public Quaternion angle;
-            public Vector3 direc;
-            public Dir(Quaternion a, Vector3 d) {
+            public Vector3 vel;
+            public Dir(Quaternion a, Vector3 v) {
                 angle = a;
-                direc = d;
+                vel = v;
             }
         };
         private Dir dir;
@@ -30,11 +30,11 @@ namespace UCM.IAV.Practica1
             // Modificar la posicion y la orientacion
             float time = Time.deltaTime;
             float half_t_sq = 0.5f * time * time;
-            this.transform.position += rb_.velocity * time + dir.direc * half_t_sq;
+            this.transform.position += rb_.velocity * time + dir.vel * half_t_sq;
 
             // y la velocidad y la rotation
             if (_v_ > radius)
-                rb_.velocity = dir.direc;
+                rb_.velocity = dir.vel;
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, dir.angle, 1);
         }
 
@@ -42,27 +42,28 @@ namespace UCM.IAV.Practica1
         private Dir getSteering(out float _v_) {
             Dir result;
             // Coger la direccion al objetivo
-            result.direc =  target.transform.position - this.transform.position;
+            result.vel =  target.transform.position - this.transform.position;
 
             // Calcular el modulo del vector para ver si es menor que el radio
-            _v_ = Mathf.Sqrt(result.direc.x * result.direc.x + 
-                result.direc.y * result.direc.y +
-                result.direc.z * result.direc.z);
+            _v_ = Mathf.Sqrt(result.vel.x * result.vel.x + 
+                result.vel.y * result.vel.y +
+                result.vel.z * result.vel.z);
             // Comprobar si estoy dentro del radio
             if (_v_ < radius) {
                 rb_.velocity = Vector3.zero;
             }
 
             // Hay que moverse hacia el objetivo en el tiempo establecido
-            result.direc /= timeToTarget;
+            result.vel /= timeToTarget;
 
             // La velocidad va a lo largo de esta direccion, a toda velocidad
-            result.direc.Normalize();
-            result.direc *= maxVelocity;
-
+            if (_v_ > maxVelocity) {
+                result.vel.Normalize();
+                result.vel *= maxVelocity;
+            }
             // Poner en la direccion que queremos que vaya
-            result.direc.y = 0;
-            result.angle = Quaternion.LookRotation(result.direc);
+            result.vel.y = 0;
+            result.angle = Quaternion.LookRotation(result.vel);
             return result;
         }
     }
