d333208873890f6fe64773614d6f6757b62c167b
Marcos Docampo <marcosdo@ucm.es>
2020-02-25 03:06:24 +0100

    Empezado el freno progresivo
    Son las 3:06 a.m. y de verdad que quiero seguir pero no puedo pensar porque los apuntes de Fede además son demasiado ambiguos

--- Movimiento/Assets/Scripts/ScriptsPractica1/Rata.cs
+++ Movimiento/Assets/Scripts/ScriptsPractica1/Rata.cs
@@ -5,8 +5,9 @@ namespace UCM.IAV.Practica1
     public class Rata : MonoBehaviour
     {
         // KINEMATIC
-        public float radius = 2.0f;
-        public float maxVelocity = 1.0f;
+        public float targetRadius = 2.0f;
+        public float brakeRadius = 4.0f;
+        public float maxSpeed = 1.0f;
         public float timeToTarget = 0.25f;
         protected struct Dir {
             public Quaternion angle;
@@ -33,8 +34,10 @@ namespace UCM.IAV.Practica1
             this.transform.position += rb_.velocity * time + dir.vel * half_t_sq;
 
             // y la velocidad y la rotation
-            if (_v_ > radius)
+            if (_v_ > targetRadius) {
+                Debug.Log("Velocidad: " + _v_);
                 rb_.velocity = dir.vel;
+            }
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, dir.angle, 1);
         }
 
@@ -44,22 +47,32 @@ namespace UCM.IAV.Practica1
             // Coger la direccion al objetivo
             result.vel =  target.transform.position - this.transform.position;
 
-            // Calcular el modulo del vector para ver si es menor que el radio
+            // Calcular el modulo del vector para ver si es menor que el radio. 
+            // El modulo es la distancia desde un punto al otro del vector
             _v_ = Mathf.Sqrt(result.vel.x * result.vel.x + 
                 result.vel.y * result.vel.y +
                 result.vel.z * result.vel.z);
-            // Comprobar si estoy dentro del radio
-            if (_v_ < radius) {
+            // Comprobar si estoy dentro del radio y pararme
+            if (_v_ <= targetRadius) {
                 rb_.velocity = Vector3.zero;
+            }
+
+            // Si estoy fuera del radio exterior, ir a maxima velocidad
+            if (_v_ <= brakeRadius && _v_ > targetRadius) {
+                result.vel *= maxSpeed * _v_ / brakeRadius;
+                float _l_ = Mathf.Sqrt(result.vel.x * result.vel.x + 
+                result.vel.y * result.vel.y +
+                result.vel.z * result.vel.z);
+                Debug.Log("Distancia: " + _l_);
             }
 
             // Hay que moverse hacia el objetivo en el tiempo establecido
             result.vel /= timeToTarget;
 
             // La velocidad va a lo largo de esta direccion, a toda velocidad
-            if (_v_ > maxVelocity) {
+            if (_v_ > maxSpeed) {
                 result.vel.Normalize();
-                result.vel *= maxVelocity;
+                result.vel *= maxSpeed;
             }
             // Poner en la direccion que queremos que vaya
             result.vel.y = 0;
