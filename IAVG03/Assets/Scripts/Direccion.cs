e146068f4c0810e8415560457ab22c52b2d8213a
Marcos Docampo <marcosdo@ucm.es>
2020-02-28 21:32:37 +0100

    Cambiado el nombre de las carpetas
    He tenido que reiniciar el ordenador para esto, Gracias Unity.

---
+++ IAVG03/Assets/Scripts/Direccion.cs
@@ -0,0 +1,36 @@
+﻿/*    
+   Copyright (C) 2020 Federico Peinado
+   http://www.federicopeinado.com
+
+   Este fichero forma parte del material de la asignatura Inteligencia Artificial para Videojuegos.
+   Esta asignatura se imparte en la Facultad de Informatica de la Universidad Complutense de Madrid (Espana).
+
+   Autor: Federico Peinado 
+   Contacto: email@federicopeinado.com
+*/
+namespace UCM.IAV.Movimiento
+{
+
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// Clase auxiliar para representar la direccion/direccionamiento con el que corregir el movimiento
+    /// </summary>
+    public class Direccion
+    {
+        public float angular;
+        public Vector3 lineal;
+        public Direccion()
+        {
+            angular = 0.0f;
+            lineal = new Vector3();
+        }
+        public Direccion(float angle, Vector3 line) {
+            angular = angle;
+            lineal = line;
+        }
+    }
+}
---
+++ IAVG03/Assets/Scripts/Seguir.cs
@@ -0,0 +1,32 @@
+﻿/*    
+   Copyright (C) 2020 Federico Peinado
+   http://www.federicopeinado.com
+
+   Este fichero forma parte del material de la asignatura Inteligencia Artificial para Videojuegos.
+   Esta asignatura se imparte en la Facultad de Informatica de la Universidad Complutense de Madrid (Espana).
+
+   Autor: Federico Peinado 
+   Contacto: email@federicopeinado.com
+*/
+namespace UCM.IAV.Movimiento
+{
+
+    /// <summary>
+    /// Clase para modelar el comportamiento de SEGUIR a otro agente
+    /// </summary>
+    public class Seguir : ComportamientoAgente
+    {
+        /// <summary>
+        /// Obtiene la direccion
+        /// </summary>
+        /// <returns></returns>
+        public override Direccion GetDireccion()
+        {
+            Direccion direccion = new Direccion();
+            direccion.lineal = objetivo.transform.position - transform.position;
+            direccion.lineal.Normalize();
+            direccion.lineal = direccion.lineal * agente.aceleracionMax;
+            return direccion;
+        }
+    }
+}
