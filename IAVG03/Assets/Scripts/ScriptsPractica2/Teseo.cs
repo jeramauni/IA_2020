0ce658b20fe0450d659cb630a5c3caaca316c1dc
Marcos Docampo <marcosdo@ucm.es>
2020-03-20 17:12:30 +0100

    Intentando moviemiento fluido

--- IAVG03/Assets/Scripts/ScriptsPractica2/Teseo.cs
+++ IAVG03/Assets/Scripts/ScriptsPractica2/Teseo.cs
@@ -6,15 +6,19 @@ namespace UCM.IAV.Practica2 {
         // Laberinto con casillas y direcciones
         public MazeLoader mazeLoader;
         // Struct con velocidad y angulo
+        public enum DIRE { UP, DOWN, LEFT, RIGHT, NONE };
         protected struct Dir {
             public Quaternion angle;
             public Vector3 vel;
             public int lastPosX, lastPosZ;
-            public Dir(Quaternion a, Vector3 v, int lx, int lz) {
+            public DIRE dirAct, dirLast;
+            public Dir(Quaternion a, Vector3 v, int lx, int lz, DIRE da, DIRE dl) {
                 angle = a;
                 vel = v;
                 lastPosX = lx;
                 lastPosZ = lz;
+                dirAct = da;
+                dirLast = dl;
             }
         };
         private Dir dir;
@@ -23,24 +27,47 @@ namespace UCM.IAV.Practica2 {
             transform.position = Vector3.zero;
             transform.rotation = default(Quaternion);
             dir.lastPosX = dir.lastPosZ = 0;
+            dir.dirLast = DIRE.NONE;
         }
 
         // Mediante transforms, modificar la posicion, direccion y orientacion
         void Update() {
-            // Debug.Log("Arriba: " + mazeLoader.mazeCells[(int)transform.position.x, (int)transform.position.z].walls[2]);
-            // Debug.Log("Abajo: " + mazeLoader.mazeCells[(int)transform.position.x, (int)transform.position.z].walls[3]);
-            // Debug.Log("Izquierda: " + mazeLoader.mazeCells[(int)transform.position.x, (int)transform.position.z].walls[0]);
-            // Debug.Log("Derecha: " + mazeLoader.mazeCells[(int)transform.position.x, (int)transform.position.z].walls[1]);
+            Debug.Log("Arriba: " + mazeLoader.mazeCells[(int)transform.position.x, (int)transform.position.z].walls[2]);
+            Debug.Log("Abajo: " + mazeLoader.mazeCells[(int)transform.position.x, (int)transform.position.z].walls[3]);
+            Debug.Log("Izquierda: " + mazeLoader.mazeCells[(int)transform.position.x, (int)transform.position.z].walls[0]);
+            Debug.Log("Derecha: " + mazeLoader.mazeCells[(int)transform.position.x, (int)transform.position.z].walls[1]);
             // Empieza el movimiento
             float time = Time.deltaTime;
             // Comprobación de la tecla espacio. Si NO esta pulsada, hacer un movimiento normal
             if (!Input.GetKeyDown(KeyCode.Space)) {
                 dir.vel.x = Input.GetAxis("Horizontal");
                 dir.vel.z = Input.GetAxis("Vertical");
-                // Codigo para seguir hacia delante
-                int posX = 0, posZ = 0;
                 // ARRIBA
-                if (dir.vel.z > 0) {
+                if (dir.vel.z > 0)
+                    dir.dirAct = DIRE.UP;
+                // ABAJO
+                else if (dir.vel.z < 0)
+                    dir.dirAct = DIRE.DOWN;
+                // IZQUIERDA
+                else if (dir.vel.x < 0)
+                    dir.dirAct = DIRE.LEFT;
+                // DERECHA
+                else if (dir.vel.x > 0)
+                    dir.dirAct = DIRE.RIGHT;
+                if (dir.dirLast != dir.dirAct && dir.dirLast != DIRE.NONE)
+                        goDirection(dir.dirLast, time);
+                else goDirection(dir.dirAct, time);
+            }
+            // En caso contrario, seguir el hilo
+            else {  }
+            // Resetear la direccion
+            dir.vel.x = dir.vel.z = 0;
+        }
+
+        void goDirection(DIRE direccion, float time) {
+            int posX = 0, posZ = 0;
+            switch(direccion) {
+                case DIRE.UP: {
                     // Si Teseo yendo aabajo sigue teniendo parte del cuerpo en la casilla de arriba, entonces aun pertenece a esa casilla
                     if (transform.position.z < (dir.lastPosZ + 1) * mazeLoader.size) {
                         posX = dir.lastPosX;
@@ -53,9 +80,8 @@ namespace UCM.IAV.Practica2 {
                         transform.position += dir.vel * time;
                         transform.rotation = new Quaternion(1, 0, 0, 0);
                     }
-                }
-                // ABAJO
-                else if (dir.vel.z < 0) {
+                } break;
+                case DIRE.DOWN: {
                     // Si Teseo yendo aabajo sigue teniendo parte del cuerpo en la casilla de arriba, entonces aun pertenece a esa casilla
                     if (transform.position.z > (dir.lastPosZ - 1) * mazeLoader.size) {
                         posX = dir.lastPosX;
@@ -68,9 +94,8 @@ namespace UCM.IAV.Practica2 {
                         transform.position += dir.vel * time;
                         transform.rotation = new Quaternion(1, 0, 0, 0);
                     }
-                }
-                // IZQUIERDA
-                else if (dir.vel.x < 0) {
+                } break;
+                case DIRE.LEFT: {
                     // Si Teseo yendo a la izquierda sigue teniendo parte del cuerpo en la casilla derecha, entonces aun pertenece a esa casilla
                     if (transform.position.x > (dir.lastPosX - 1) * mazeLoader.size) {
                         posX = dir.lastPosX;
@@ -83,9 +108,8 @@ namespace UCM.IAV.Practica2 {
                         transform.position += dir.vel * time;
                         transform.rotation = new Quaternion(1, 0, 0, 0);
                     }
-                }
-                // DERECHA
-                else if (dir.vel.x > 0) {
+                } break;
+                case DIRE.RIGHT: {
                     // Si Teseo yendo a la derecha sigue teniendo parte del cuerpo en la casilla izquierda, entonces aun pertenece a esa casilla
                     if (transform.position.x < (dir.lastPosX + 1) * mazeLoader.size) {
                         posX = dir.lastPosX;
@@ -98,12 +122,8 @@ namespace UCM.IAV.Practica2 {
                         transform.position += dir.vel * time;
                         transform.rotation = new Quaternion(1, 0, 0, 0);
                     }
-                }
+                } break;
             }
-            // En caso contrario, seguir el hilo
-            else {  }
-            // Resetear la direccion
-            dir.vel.x = dir.vel.z = 0;
         }
     }
 }
