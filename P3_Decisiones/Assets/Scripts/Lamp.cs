b6d19073f3f9301f4ad5acaebb8f7794d0fa67dc
Jesús Ramos Rodríguez <jeramo02@ucm.es>
2020-04-27 22:31:47 +0200

    Lamp and lever script
    Also added lever sound

---
+++ P3_Decisiones/Assets/Scripts/Lamp.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+    private bool fall;
+    private Behaviour halo;
+
+    public void Fall()
+    {
+        if (!fall)
+        {
+            //On ground position
+            this.transform.position = new Vector3(transform.position.x, 1.4f, transform.position.z);
+            this.transform.Rotate(-100, 0, 0);
+
+            //Turn off directional light
+            transform.GetChild(1).gameObject.SetActive(false);
+
+            //Turn halo off
+            halo.enabled = false;
+
+
+            fall = true;
+        }
+
+    }
+    public void Repair()
+    {
+        if (fall)
+        {
+            //On ceiling position
+            this.transform.position = new Vector3(transform.position.x, 10.0f, transform.position.z);
+            this.transform.Rotate(100, 0, 0);
+
+            //Turn directional light back on
+            transform.GetChild(1).gameObject.SetActive(true);
+
+            //Turn halo on
+            halo.enabled = true;
+
+            fall = false;
+        }
+    }
+
+    //Getters and Setters
+    public bool FalledDown() { return fall; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initial state
+        fall = false;
+        halo = (Behaviour)transform.GetChild(0).gameObject.GetComponent("Halo");
+    }
+}
---
+++ P3_Decisiones/Assets/Scripts/Lever.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject lamp;        //Lamp afeccted by this lever
+    private Lamp lamp_;
+
+    void Start()
+    {
+        lamp_ = lamp.GetComponent<Lamp>();
+    }
+    private void Toggle()
+    {
+        lamp_.Fall();
+    }
+}
