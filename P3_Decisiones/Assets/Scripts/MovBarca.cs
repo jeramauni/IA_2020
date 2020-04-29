01414233a0c22b36458682f0fc3da056b29086c1
Alvaro Serna <alsern01@ucm.es>
2020-04-29 20:51:24 +0200

    hecho el sistema de barcas

---
+++ P3_Decisiones/Assets/Scripts/MovBarca.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovBarca : MonoBehaviour
+{
+    public NavMeshLink link;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            transform.SetParent(collision.collider.transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            transform.SetParent(null);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Trigger")
+        {
+            transform.SetParent(null);
+            transform.localRotation = Quaternion.identity;
+
+            Vector3 aux = link.startPoint;
+            link.startPoint = link.endPoint;
+            link.endPoint = aux;
+        }
+    }
+}
--- P3_Decisiones/Assets/Scripts/MoveToClickPoint.cs
+++ P3_Decisiones/Assets/Scripts/MoveToClickPoint.cs
@@ -3,16 +3,24 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class MoveToClickPoint : MonoBehaviour {
-    NavMeshAgent agent;
-    void Start() {
+    
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
         agent = GetComponent<NavMeshAgent>();
     }
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
+
             RaycastHit hit;
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
-                agent.destination = hit.point;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) {
+                agent.SetDestination(hit.point);
             }
         }
+
+        
     }
 }
