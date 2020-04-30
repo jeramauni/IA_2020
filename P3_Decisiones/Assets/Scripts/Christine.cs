959a8a951b73205fac2de83c00b83fc2bb36d9fd
Jesús Ramos Rodríguez <jeramo02@ucm.es>
2020-04-30 12:45:08 +0200

    Cantante finalizada

---
+++ P3_Decisiones/Assets/Scripts/Christine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Christine : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void LateUpdate()
+    {
+
+        Vector3 v = agent.velocity.normalized;
+        v.y = 0;
+        Vector3 f = this.transform.position + v;
+        this.transform.LookAt(f);
+    }
+}
