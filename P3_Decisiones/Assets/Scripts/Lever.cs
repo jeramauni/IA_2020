2834ba07c3fee2ad9718ec9478c8bca3daeafa3e
Marcos Docampo <marcosdo@ucm.es>
2020-05-01 20:10:00 +0200

    Comentado y compactado el script Lever.cs

--- P3_Decisiones/Assets/Scripts/Lever.cs
+++ P3_Decisiones/Assets/Scripts/Lever.cs
@@ -2,66 +2,51 @@
 
 public class Lever : MonoBehaviour
 {
+    // Lampara afectada por esta palanca
     [SerializeField]
-    private GameObject lamp;        //Lamp afeccted by this lever
+    private GameObject lamp;        
     private Lamp lamp_;
-
     private GameObject on_button;
     private GameObject off_button;
-
     private bool enemy_inside;
     private bool status;
-
-    void Awake()
-    {
+    // Coger cada uno de los botones
+    void Awake() {
         lamp_ = lamp.GetComponent<Lamp>();
         on_button = this.gameObject.transform.GetChild(1).gameObject.transform.GetChild(0).gameObject;
         off_button = this.gameObject.transform.GetChild(1).gameObject.transform.GetChild(1).gameObject;
     }
-
-    void Start()
-    {
+    // Activar el verde y desactivar el rojo
+    void Start() {
         enemy_inside = false;
         //Starts with status on
         status = true;
         on_button.SetActive(true);
         off_button.SetActive(false);
     }
-
-    //Detection of player position related to area of lever influence
-    private void OnTriggerEnter(Collider other)
-    {
+    // Deteccion del agente en el area de influencia
+    private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Enemy"))
-        {
             enemy_inside = true;
-        }
     }
-
-    private void OnTriggerExit(Collider other)
-    {
+    // Detecction de la salida del agente del area de infuencia
+    private void OnTriggerExit(Collider other) {
         if (other.CompareTag("Enemy"))
-        {
             enemy_inside = false;
-        }
     }
-
-    public void TurnOff()
-    {
-        //If green button is turned on
-        if (status && enemy_inside)
-        {
-            //Sets the off status and makes the lamp fall
+    // Cambiar de boton al rojo (apagar)
+    public void TurnOff() {
+        // Si el boton verde esta pulsado y el agente esta dentro
+        if (status && enemy_inside) {
+            // Cambiar el estatus a caida y llamar a la caida de la lampara
             on_button.SetActive(false);
             off_button.SetActive(true);
             status = false;
             lamp_.Fall();
         }
     }
-
-    public void TurnOn()
-    {
-        //If red button is turned on
-        //Sets the ready(on) status
+    // Cambiar de boton al verde (encender)
+    public void TurnOn() {
         on_button.SetActive(true);
         off_button.SetActive(false);
         status = true;
